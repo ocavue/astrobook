1bf80b7da4197b9f721f101a9711a39a5331824a
ocavue <ocavue@users.noreply.github.com>
2024-08-08 19:03:09 +1000

    feat: use unocss to style components (#1)

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from 'tsup'
 const config: unknown = defineConfig({
   entry: {
     index: 'src/index.ts',
-    'components/navigation': 'src/components/navigation.ts',
-    'components/story': 'src/components/story.ts',
-    'components/app': 'src/components/app.ts',
   },
   format: ['esm'],
   clean: true,
