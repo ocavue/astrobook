6a533ba666c71da0d166a9433fd225521622a0c3
ocavue <ocavue@gmail.com>
2024-08-09 03:40:17 +1000

    fix: don't apply `client:load` for astro components

---
+++ packages/core/src/client.ts
@@ -0,0 +1,25 @@
+export function isAstroComponent(module: {
+  default?: { component?: unknown }
+}) {
+  try {
+    const component = module.default?.component
+    if (!component) return false
+
+    const comp = component as Record<string, unknown>
+
+    if (comp.isAstroComponentFactory) {
+      return true
+    }
+
+    const moduleId = comp.moduleId
+    if (
+      moduleId &&
+      typeof moduleId === 'string' &&
+      moduleId.toLowerCase().endsWith('.astro')
+    ) {
+      return true
+    }
+  } catch {
+    return false
+  }
+}
--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup'
 const config: unknown = defineConfig({
   entry: {
     index: 'src/index.ts',
+    client: 'src/client.ts',
   },
   format: ['esm'],
   clean: true,
