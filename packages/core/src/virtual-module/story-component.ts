1492e65abfc6241e0a154b3fede8c105b0bf90bb
ocavue <ocavue@users.noreply.github.com>
2024-08-09 03:41:53 +1000

    fix: don't apply `client:load` for astro components (#5)

--- packages/core/src/virtual-module/story-component.ts
+++ packages/core/src/virtual-module/story-component.ts
@@ -1,19 +1,34 @@
 import { collectStoryEntries } from '../story-file/collect-story-entries'
 
 export async function loadStoryComponent(): Promise<string> {
-  const scriptCode: string[] = []
+  const importCode: string[] = []
   const templateCode: string[] = []
 
   const stories = await collectStoryEntries(process.cwd())
 
   for (const [index, story] of stories.entries()) {
-    scriptCode.push(`import * as module${index} from '${story.modulePath}';`)
+    importCode.push(`import * as m${index} from '${story.modulePath}';`)
     templateCode.push(
-      `{ id === '${story.id}' && <module${index}.default.component { ...module${index}?.['${story.name}']?.args } client:load /> }`,
+      [
+        `{`,
+        `id === '${story.id}' ?`,
+        `isAstroComponent(m${index}) ?`,
+        `(<m${index}.default.component { ...m${index}?.['${story.name}']?.args } />) :`,
+        `(<m${index}.default.component { ...m${index}?.['${story.name}']?.args } client:load />) :`,
+        `null`,
+        `}`,
+      ].join(' '),
     )
   }
 
-  scriptCode.push('const id = Astro.props.story;')
+  const result = [
+    `---`,
+    `import { isAstroComponent } from 'astrobook/client';`,
+    ...importCode,
+    'const id = Astro.props.story;',
+    `---`,
+    ...templateCode,
+  ].join('\n')
 
-  return [`---`, ...scriptCode, `---`, ...templateCode].join('\n')
+  return result
 }
