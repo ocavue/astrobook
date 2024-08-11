6834cc2b2340e70664b59cfd205d8e6320e8d396
ocavue <ocavue@gmail.com>
2024-08-11 11:48:18 +1000

    Break format

--- packages/core/src/utils/base.ts
+++ packages/core/src/utils/base.ts
@@ -4,6 +4,9 @@ import { ensureLeadingSlash, stripSlashes } from './path'
 
 export function getPathWithBase(path: string, base: string): string {
   return ensureLeadingSlash(
+
+
+    
     [base, path].map(stripSlashes).filter(Boolean).join('/'),
   )
 }
