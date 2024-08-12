2e4f7cebf13bc8d3af42594663b32f88d842527e
ocavue <ocavue@gmail.com>
2024-08-12 19:19:58 +1000

    add comment

--- playground/playwright.config.ts
+++ playground/playwright.config.ts
@@ -18,7 +18,6 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -71,9 +70,12 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command:
+      process.env.ASTROBOOK_TEST_MODE === 'preview'
+        ? 'pnpm run preview'
+        : 'pnpm run dev',
+    url: 'http://localhost:4321',
+    reuseExistingServer: !process.env.CI,
+  },
 })
