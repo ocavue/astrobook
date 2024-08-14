1b68614e9fcff8daa6a44918e894e515e90f3d10
ocavue <ocavue@gmail.com>
2024-08-14 20:54:30 +1000

    docs: add lit example

---
+++ playground/src/components/lit/LitCounter.stories.ts
@@ -0,0 +1,15 @@
+import { LitCounter } from './LitCounter'
+
+export default {
+  component: LitCounter,
+}
+
+export const Default = {
+  args: {},
+}
+
+export const LargeStep = {
+  args: {
+    step: 5,
+  },
+}
