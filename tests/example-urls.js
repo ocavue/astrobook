7689624cdd214aeb57b03c77638261234d882abf
ocavue <ocavue@users.noreply.github.com>
2024-12-08 20:11:29 +1100

    docs: add unocss example (#79)

--- tests/example-urls.js
+++ tests/example-urls.js
@@ -2,4 +2,5 @@
 export const EXAMPLE_URLS = /** @type {const} */ ({
   'example-playground': 'http://localhost:4321',
   'example-mixed': 'http://localhost:4302/base',
+  'example-unocss': 'http://localhost:4303',
 })
