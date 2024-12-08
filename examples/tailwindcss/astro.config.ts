5c4baba9ee22e0f415f960ec2115439c48c4a8b1
ocavue <ocavue@gmail.com>
2024-12-08 20:15:47 +1100

    docs: add tailwindcss example

---
+++ examples/tailwindcss/astro.config.ts
@@ -0,0 +1,12 @@
+import preact from '@astrojs/preact'
+import { defineConfig } from 'astro/config'
+import astrobook from 'astrobook'
+
+// https://astro.build/config
+export default defineConfig({
+  server: {
+    port: 4303,
+  },
+
+  integrations: [preact(), astrobook({}), ],
+})
