dd0593b455b705d777b6714978405e77a47e4046
ocavue <ocavue@gmail.com>
2024-12-08 20:16:38 +1100

    update test

--- tests/example-urls.js
+++ tests/example-urls.js
@@ -3,4 +3,5 @@ export const EXAMPLE_URLS = /** @type {const} */ ({
   'example-playground': 'http://localhost:4321',
   'example-mixed': 'http://localhost:4302/base',
   'example-unocss': 'http://localhost:4303',
+  'example-tailwindcss': 'http://localhost:4304',
 })
---
+++ tests/tailwindcss/index.test.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test'
+
+import { EXAMPLE_URLS } from '../example-urls'
+
+const BASE_URL = EXAMPLE_URLS['example-tailwindcss']
+
+test('tailwindcss example', async ({ page }) => {
+  await test.step('Select the story', async () => {
+    await page.goto(BASE_URL)
+    const button = page.locator('a', { hasText: 'LargeStep' })
+    await expect(button).toBeVisible()
+    await button.click()
+
+    await page.waitForURL(
+      `${BASE_URL}/dashboard/src/stories/preact-counter/large-step`,
+    )
+  })
+
+  await test.step('Check the style', async () => {
+    const button = page.locator('button', { hasText: '+' })
+    await expect(button).toHaveClass(/size-\[100px]/)
+    const box = await button.boundingBox()
+    expect(box).not.toBeNull()
+    expect(Math.round(box?.width ?? 0)).toBe(100)
+    expect(Math.round(box?.height ?? 0)).toBe(100)
+  })
+})
