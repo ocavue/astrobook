9b239dd1589bdce7d206e5e2b6f0b7038b6fc61f
ocavue <ocavue@gmail.com>
2024-12-13 16:00:18 +1100

    add changelog

--- packages/core/src/virtual-module/virtual-module-ids.ts
+++ packages/core/src/virtual-module/virtual-module-ids.ts
@@ -2,8 +2,8 @@ export const STORY_MODULES_ID = 'virtual:astrobook/story-modules.mjs'
 export const STORY_MODULES_RESOLVED_ID =
   '__virtual_astrobook_story_modules__.mjs'
 
-export const USER_CONFIG_ID = 'virtual:astrobook/global-config.mjs'
-export const USER_CONFIG_RESOLVED_ID = '__virtual_astrobook_user_config__.mjs'
+export const GLOBAL_CONFIG_ID = 'virtual:astrobook/global-config.mjs'
+export const GLOBAL_CONFIG_RESOLVED_ID = '__virtual_astrobook_user_config__.mjs'
 
 export const COMPONENT_HEAD_ID = 'virtual:astrobook/components/head.mjs'
 export const COMPONENT_HEAD_RESOLVED_ID = '__virtual_astrobook_COMPONENT_HEAD__.mjs'
--- packages/core/src/virtual-module/vite-plugin.ts
+++ packages/core/src/virtual-module/vite-plugin.ts
@@ -7,8 +7,8 @@ import {
   COMPONENT_HEAD_RESOLVED_ID,
   STORY_MODULES_ID,
   STORY_MODULES_RESOLVED_ID,
-  USER_CONFIG_ID,
-  USER_CONFIG_RESOLVED_ID
+  GLOBAL_CONFIG_ID,
+  GLOBAL_CONFIG_RESOLVED_ID
 } from './virtual-module-ids'
 
 export function createVirtualFilesPlugin(
@@ -21,8 +21,8 @@ export function createVirtualFilesPlugin(
       switch (id) {
         case STORY_MODULES_ID:
           return STORY_MODULES_RESOLVED_ID
-        case USER_CONFIG_ID:
-          return USER_CONFIG_RESOLVED_ID
+        case GLOBAL_CONFIG_ID:
+          return GLOBAL_CONFIG_RESOLVED_ID
         case COMPONENT_HEAD_ID:
           return COMPONENT_HEAD_RESOLVED_ID
       }
@@ -31,7 +31,7 @@ export function createVirtualFilesPlugin(
       switch (id) {
         case STORY_MODULES_RESOLVED_ID:
           return loadStoryModules(rootDir)
-        case USER_CONFIG_RESOLVED_ID:
+        case GLOBAL_CONFIG_RESOLVED_ID:
           return `const config = ${JSON.stringify(config)}; export default config;`
         case COMPONENT_HEAD_RESOLVED_ID:
           return `export { default } from ${JSON.stringify(config.head)};`;
