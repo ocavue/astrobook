8e1e6444699b7b6999aba7fb4592da1d8f089d27
ocavue <ocavue@gmail.com>
2025-03-19 13:47:15 +1100

    format

--- examples/custom-head/src/env.d.ts
+++ examples/custom-head/src/env.d.ts
@@ -2,17 +2,15 @@
 /// <reference types="astro/client" />
 
 declare module '*.vue' {
-    import type { DefineComponent } from 'vue'
+  import type { DefineComponent } from 'vue'
 
-    const component: DefineComponent
-    export default component
+  const component: DefineComponent
+  export default component
 }
 
 declare module '*.astro' {
-    import type { AstroComponentFactory } from 'astro/runtime/server/index.js'
-  
-    const content: AstroComponentFactory
-    export default content
-  }
-  
+  import type { AstroComponentFactory } from 'astro/runtime/server/index.js'
 
+  const content: AstroComponentFactory
+  export default content
+}
