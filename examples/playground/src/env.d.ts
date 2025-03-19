ae241f7e270503d796a60e498ef599ecfe24b9a4
ocavue <ocavue@gmail.com>
2025-03-19 13:45:18 +1100

    fix eslint

--- examples/playground/src/env.d.ts
+++ examples/playground/src/env.d.ts
@@ -1,5 +1,18 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
-declare module '*.vue'
-declare module '*.astro'
+declare module '*.vue' {
+    import type { DefineComponent } from 'vue'
+
+    const component: DefineComponent
+    export default component
+}
+
+declare module '*.astro' {
+    import type { AstroComponentFactory } from 'astro/runtime/server/index.js'
+  
+    const content: AstroComponentFactory
+    export default content
+  }
+  
+
