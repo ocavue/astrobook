221a4f0fce3a916c192410c177d7817382128304
Jordan Sweeting <slurpyb@fastmail.com>
2025-05-28 22:15:57 +0800

    Add tests for all button variants

--- tests/pandacss/index.test.ts
+++ tests/pandacss/index.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'
+import { test, expect, type Page } from '@playwright/test'
 
 import { EXAMPLE_URLS } from '../example-urls'
 
@@ -7,28 +7,107 @@ const BASE_URL = EXAMPLE_URLS['example-pandacss']
 // For unknown reasons, this test fails on Windows.
 test.skip(process.platform === 'win32', 'Skip on Windows')
 
-test('pandacss example', async ({ page }) => {
-  await test.step('Select the ghost button story', async () => {
-    await page.goto(BASE_URL)
-    const button = page.locator('a', { hasText: 'Ghost' })
-    await expect(button).toBeVisible()
-    await button.click()
+async function locateButtonStory(page: Page, storyName: string) {
+  await page.goto(BASE_URL)
+  const button = page.locator('a', { hasText: storyName })
+  await expect(button).toBeVisible()
+  await button.click()
 
-    await page.waitForURL(`${BASE_URL}/dashboard/button/ghost`)
+  await page.waitForURL(
+    `${BASE_URL}/dashboard/button/${storyName.toLowerCase()}`,
+  )
+}
+
+async function checkButtonVariantStyles(
+  page: Page,
+  name: string,
+  color: string,
+  backgroundColor: string,
+  hoverBackgroundColor: string,
+) {
+  const button = page.locator('button', { hasText: name })
+  await expect(button).toBeVisible()
+  await expect(button).toHaveClass(/button/)
+  await expect(button).toHaveClass(/button--size_md/)
+  await expect(button).toHaveClass(
+    new RegExp(`button--variant_${name.toLowerCase()}`),
+  )
+
+  await expect(button).toHaveCSS('color', color)
+  await expect(button).toHaveCSS('background-color', backgroundColor)
+  await button.hover()
+  await page.waitForTimeout(200)
+  await expect(button).toHaveCSS('background-color', hoverBackgroundColor)
+}
+
+test('pandacss example', async ({ page }) => {
+  await test.step('Select the solid button story', async () => {
+    await locateButtonStory(page, 'Solid')
+  })
+
+  await test.step('Check the solid button styles', async () => {
+    await checkButtonVariantStyles(
+      page,
+      'Solid',
+      'rgb(255, 255, 255)',
+      'rgb(233, 61, 130)',
+      'rgb(223, 52, 120)',
+    )
+  })
+
+  await test.step('Select the link button story', async () => {
+    await locateButtonStory(page, 'Link')
+  })
+
+  await test.step('Check the link button styles', async () => {
+    await checkButtonVariantStyles(
+      page,
+      'Link',
+      'rgb(28, 32, 36)',
+      'rgba(0, 0, 0, 0)',
+      'rgba(0, 0, 0, 0)',
+    )
+  })
+
+  await test.step('Select the outline button story', async () => {
+    await locateButtonStory(page, 'Outline')
+  })
+
+  await test.step('Check the outline button styles', async () => {
+    await checkButtonVariantStyles(
+      page,
+      'Outline',
+      'rgb(28, 32, 36)',
+      'rgba(0, 0, 0, 0)',
+      'rgba(0, 0, 85, 0.024)',
+    )
+  })
+
+  await test.step('Select the subtle button story', async () => {
+    await locateButtonStory(page, 'Subtle')
+  })
+
+  await test.step('Check the subtle button styles', async () => {
+    await checkButtonVariantStyles(
+      page,
+      'Subtle',
+      'rgb(28, 32, 36)',
+      'rgba(0, 0, 51, 0.06)',
+      'rgba(0, 0, 45, 0.09)',
+    )
+  })
+
+  await test.step('Select the ghost button story', async () => {
+    await locateButtonStory(page, 'Ghost')
   })
 
   await test.step('Check the ghost button styles', async () => {
-    const button = page.locator('button', { hasText: 'Ghost' })
-    await expect(button).toBeVisible()
-    await expect(button).toHaveClass(/button/)
-    await expect(button).toHaveClass(/button--variant_ghost/)
-    await expect(button).toHaveClass(/button--size_md/)
-    await expect(button).toHaveCSS('color', 'rgb(28, 32, 36)')
-
-    await expect(button).toHaveCSS('height', '40px')
-    await expect(button).toHaveCSS('min-width', '40px')
-    await button.hover()
-    await page.waitForTimeout(200)
-    await expect(button).toHaveCSS('background-color', 'rgba(0, 0, 51, 0.06)')
+    await checkButtonVariantStyles(
+      page,
+      'Ghost',
+      'rgb(28, 32, 36)',
+      'rgba(0, 0, 0, 0)',
+      'rgba(0, 0, 51, 0.06)',
+    )
   })
 })
