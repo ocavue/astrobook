2e1b99bed3bfc1bc152c35414e9f8274d232fc96
Jordan Sweeting <slurpyb@fastmail.com>
2025-06-02 07:33:34 +0800

    Move font-size tokens into text styles to reduce complexity

--- examples/pandacss/panda.config.ts
+++ examples/pandacss/panda.config.ts
@@ -46,44 +46,16 @@ export default defineConfig({
   theme: {
     extend: {
       textStyles: {
-        xs: { value: { fontSize: 'xs', lineHeight: '1.125rem' } },
-        sm: { value: { fontSize: 'sm', lineHeight: '1.25rem' } },
-        md: { value: { fontSize: 'md', lineHeight: '1.5rem' } },
-        lg: { value: { fontSize: 'lg', lineHeight: '1.75rem' } },
-        xl: { value: { fontSize: 'xl', lineHeight: '1.875rem' } },
-        '2xl': { value: { fontSize: '2xl', lineHeight: '2rem' } },
-        '3xl': { value: { fontSize: '3xl', lineHeight: '2.375rem' } },
-        '4xl': {
-          value: {
-            fontSize: '4xl',
-            lineHeight: '2.75rem',
-            letterSpacing: '-0.02em',
-          },
-        },
-        '5xl': {
-          value: {
-            fontSize: '5xl',
-            lineHeight: '3.75rem',
-            letterSpacing: '-0.02em',
-          },
-        },
-        '6xl': {
-          value: {
-            fontSize: '6xl',
-            lineHeight: '4.5rem',
-            letterSpacing: '-0.02em',
-          },
-        },
-        '7xl': {
-          value: {
-            fontSize: '7xl',
-            lineHeight: '5.75rem',
-            letterSpacing: '-0.02em',
-          },
-        },
+        '2xs': { value: { fontSize: '0.5rem', lineHeight: '0.75rem' } },
+        xs: { value: { fontSize: '0.75rem', lineHeight: '1.125rem' } },
+        sm: { value: { fontSize: '0.875rem', lineHeight: '1.25rem' } },
+        md: { value: { fontSize: '1rem', lineHeight: '1.5rem' } },
+        lg: { value: { fontSize: '1.125rem', lineHeight: '1.75rem' } },
+        xl: { value: { fontSize: '1.25rem', lineHeight: '1.875rem' } },
+        '2xl': { value: { fontSize: '1.5rem', lineHeight: '2rem' } },
+        '3xl': { value: { fontSize: '1.875rem', lineHeight: '2.375rem' } },
       },
       tokens: {
-        fontSizes: fontSizeTokens,
         colors: colorTokens,
       },
       semanticTokens: {
