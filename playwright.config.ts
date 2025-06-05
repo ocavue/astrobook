2e6113e3b9f44f9bbe354e38d155afcda58bcb2d
ocavue <ocavue@gmail.com>
2025-06-05 22:44:19 +1000

    make test faster

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,8 +20,6 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 4 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
