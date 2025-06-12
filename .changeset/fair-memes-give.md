3376a74b2021640688e2a42abbb4333fcb210101
ocavue <ocavue@users.noreply.github.com>
2025-06-13 00:20:02 +1000

    chore: update dependencies (#130)

---
+++ .changeset/fair-memes-give.md
@@ -0,0 +1,5 @@
+---
+'@astrobook/ui': patch
+---
+
+Update dependencies.
--- packages/ui/uno.config.ts
+++ packages/ui/uno.config.ts
@@ -27,6 +27,11 @@ const config: unknown = defineConfig({
   shortcuts: {
     'astrobook-focus-ring':
       'outline-gray-900 dark:outline-gray-100 outline-0 focus-visible:outline-2 outline-offset-2 focus-visible:z-20',
+
+    'astrobook-sidebar-button': [
+      'text-size-base block cursor-pointer rounded border-0 bg-transparent p-2 font-semibold text-gray-500 no-underline transition hover:scale-110 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200',
+      'astrobook-focus-ring',
+    ],
   },
 })
 
