e0da4d91e14362dda599b4402a3d846d136a9241
ocavue <ocavue@gmail.com>
2025-08-01 17:44:09 +1000

    remove

--- examples/playground/src/components/astro/AstroCounter.stories.ts
+++ examples/playground/src/components/astro/AstroCounter.stories.ts
@@ -1,8 +1,5 @@
 import type { ComponentProps } from 'astro/types'
 
-import GreenBorderDecorator from '../decorators/GreenBorderDecorator.astro'
-import RedBorderDecorator from '../decorators/RedBorderDecorator.astro'
-
 import AstroCounter from './AstroCounter.astro'
 
 type AstroCounterProps = ComponentProps<typeof AstroCounter>
@@ -19,18 +16,4 @@ export const LargeStep = {
   args: {
     step: 5,
   } satisfies AstroCounterProps,
-}
-
-export const RedBorder = {
-  args: {
-    step: 1,
-  } satisfies AstroCounterProps,
-  decorators: [{ component: RedBorderDecorator, props: { size: '2px' } }],
-}
-
-export const GreenBorder = {
-  args: {
-    step: 1,
-  } satisfies AstroCounterProps,
-  decorators: [{ component: GreenBorderDecorator }],
 }
