af9407bb6b820177e4ef723319f019b599cdd5cb
ocavue <ocavue@gmail.com>
2025-11-06 20:32:10 +1100

    chore(deps): update dependencies

--- packages/ui/src/components/theme-message.ts
+++ packages/ui/src/components/theme-message.ts
@@ -1,4 +1,4 @@
-import { setTheme } from 'astro-theme-toggle'
+import { setTheme } from 'astro-theme-toggle/client'
 
 if (typeof window !== 'undefined') {
   // Add message event listener to toggle the theme
