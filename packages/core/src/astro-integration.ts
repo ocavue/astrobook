961f07bc3f79fc564d6410d387f5ce70d3839686
Ivan Che <88876408+gVguy@users.noreply.github.com>
2025-11-07 15:40:02 +0100

    fix: support astro `--root` option (#191)

--- packages/core/src/astro-integration.ts
+++ packages/core/src/astro-integration.ts
@@ -32,7 +32,10 @@ export function createAstrobookIntegration(
         logger,
         command,
       }) => {
-        const rootDir = path.resolve(resolvedOptions.directory)
+        const rootDir = path.resolve(
+          fileURLToPath(config.root),
+          resolvedOptions.directory,
+        )
 
         const astroBase = config.base || ''
 
@@ -120,12 +123,9 @@ export function createAstrobookIntegration(
 
         logger.debug(`Injecting routes`)
         for (const route of routes.values()) {
-          const entrypoint = path.normalize(
-            path.relative('.', route.entrypoint),
-          )
           injectRoute({
             pattern: route.pattern,
-            entrypoint,
+            entrypoint: route.entrypoint,
             prerender: true,
           })
         }
