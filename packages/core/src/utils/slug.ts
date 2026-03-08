113a4dee59f80cbeebbc16e9dfefd814db85ce9d
ocavue <ocavue@gmail.com>
2026-03-08 17:15:16 +1100

    Revert "improve msg"
    This reverts commit de8bf3d686ecbf1b431f1bb82fc05b670d623f9e.

--- packages/core/src/utils/slug.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Replaces all non-alphanumeric characters with underscores and converts to lowercase.
- */
-export function slug(s: string) {
-    return s.replaceAll(/[^\dA-Za-z]+/g, '_').replaceAll(/^_+|_+$/g, '').toLowerCase();
-}
