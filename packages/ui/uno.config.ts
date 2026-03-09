e4602ce0ad6a3f45de6b2d10b13210a3db99bd26
ocavue <ocavue@gmail.com>
2026-03-09 17:38:07 +1100

    refactor: use `<ClientRouter />` to persist sidebar state

--- packages/ui/uno.config.ts
+++ packages/ui/uno.config.ts
@@ -32,6 +32,31 @@ const config: unknown = defineConfig({
       'text-size-base block cursor-pointer rounded border-0 bg-transparent p-2 font-semibold text-gray-500 no-underline transition hover:scale-110 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200',
       'astrobook-focus-ring',
     ],
+
+    'astrobook-sidebar-story-link-inactive': [
+      // text color
+      'text-gray-600',
+      'hover:text-gray-800',
+      'dark:text-gray-400',
+      'dark:hover:text-gray-100',
+
+      // background color
+      'hover:bg-gray-200',
+      'dark:hover:bg-gray-700',
+    ],
+    'astrobook-sidebar-story-link-active': [
+      // text color
+      'text-white',
+      'hover:text-white',
+      'dark:text-gray-900',
+      'dark:hover:text-gray-900',
+
+      // background color
+      'bg-gray-800',
+      'hover:bg-gray-700',
+      'dark:bg-gray-200',
+      'dark:hover:bg-gray-200',
+    ],
   },
 })
 
